9806e6ac0648f8dfe71de236f7845de247a8f6e6
Hammad Gulzar <hammadgulzar146@gmail.com>
2025-12-31 16:57:14 +0500

    DOM

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var scores, roundScore, dice;
+var scores,activePlayer, roundScore, dice;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
-
+//This code specifically generates an integer (whole number)
 dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+document.querySelector('#current--0').textContent = dice;
+// This will display whatever text is inside the element with the ID score--0.
+var x = document.querySelector('#score--0').textContent;
+console.log(x);
+//Setting display to 'none' completely removes the element from the page layout so it takes up no space.
+document.querySelector('.dice').style.display = 'none';
